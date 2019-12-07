3172514ff3a4e1c233fd92e1b5ce66dd10863725
DATBOI <alexsimons9999@gmail.com>
2019-12-07 07:04:03 -0600

    migrating to typescript

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+const serverless = require('serverless-http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const application = express();
+
+application.use(bodyParser.json({strict: false}));
+application.use(bodyParser.urlencoded({extended: true}));
+application.post('/', commandHandler);
+
+application.use((request, response) => {
+  response.status(418).send(teapot);
+});
+
+module.exports.handler = serverless(application);
