d23eec9e72fde9939db7dfa31e16804b1a621989
DATBOI <alexsimons9999@gmail.com>
2019-12-07 07:39:28 -0600

    Yay, i can post

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import ImmaTeapot from "./ImmaTeapot";
+import axios from 'axios';
+import ImmaTeapot from './ImmaTeapot';
+
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const express = require('express');
@@ -7,7 +9,13 @@ const application = express();
 application.use(bodyParser.json({strict: false}));
 application.use(bodyParser.urlencoded({extended: true}));
 application.post('/slack/error', (request, response) => {
-  response.status(200);
+  const errorUrl = process.env.SLACK_URL;
+  console.log(errorUrl);
+  axios.post(errorUrl, {
+    text: request.body,
+  })
+    .then(() => response.status(204).send())
+    .catch(() => response.status(204).send());
 });
 
 application.use((request, response) => {
